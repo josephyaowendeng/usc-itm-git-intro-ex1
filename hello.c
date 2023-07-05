95ee2e6a10cffdf94d41c023232bc623d66e68cd
josephyaowendeng <josephdeng@g2.usc.edu.tw>
2023-07-05 17:12:07 +0800

    Add command line names

--- hello.c
+++ hello.c
@@ -3,6 +3,6 @@
 int main(int argc, char *argv[])
 {
     int i;
-    for (i = 0; i < argc; i++)
-        printf("%d: argv[%d]='%s'\n", i, i, argv[i]);
+    for (i = 1; i < argc; i++)
+        printf("Hello %s\n", argv[i]);
 }
